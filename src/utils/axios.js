7566e5dcd4294b3388372107379d17f2d41fc9bb
Sahil Kalkal <sahilkalkal108@gmail.com>
2025-04-01 10:41:45 +0530

    initial deploy

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "https://e-commerce-backend-5oow.onrender.com/api",
+    baseURL: "http://localhost:5000/api",
     withCredentials: true
 })
 
